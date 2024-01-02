85a808799524225c6a80bc82bcc7b26a00ec5629
imgoury02 <gourysindhu002@gmail.com>
2024-01-02 12:36:23 +0530

    final

--- js/script.js
+++ js/script.js
@@ -12,9 +12,4 @@
       
       });	
       
-      // Cufon
-      Cufon.replace('h1, h2, h3, h4, h5, h6, .content .mainbar a.com, .content .mainbar a.rm', { hover: true });
-      //Cufon.replace('h1', { color: '-linear-gradient(#fff, #ffaf02)'});
-      //Cufon.replace('h1 small', { color: '#8a98a5'});
       
-      // ]]>
